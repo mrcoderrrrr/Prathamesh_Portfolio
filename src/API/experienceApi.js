29cbd2f2873e1a6f1ecd4cd28f5667cbe3fcd8a1
Prathamesh Satardekar <84144618+mrcoderrrrr@users.noreply.github.com>
2022-03-06 13:56:46 +0530

    Update experienceApi.js
    intership details

--- src/API/experienceApi.js
+++ src/API/experienceApi.js
@@ -1,10 +1,16 @@
 const expApi = [
   {
     company: "Think Gestalt",
-    role: "Andriod Developer",
+    role: "Andriod Developer(Java)",
     duration: "Aug 2021 - Oct 2021",
     
   },
+  {
+    company: "VGD Technology",
+    role: "Andriod Developer(Kotlin)",
+    duration: "Jan 2022 - Present",
+    
+  }
 ];
 
 export default expApi;
